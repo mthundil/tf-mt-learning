78e784843c893b700254eafff733a2dc00b0e1d5
mthundil <85497016+mthundil@users.noreply.github.com>
2025-04-11 16:08:38 -0400

    updated provider.tf

--- provider.tf
+++ provider.tf
@@ -8,8 +8,8 @@ terraform {
 
   # Update this block with the location of your terraform state file
   backend "azurerm" {
-    resource_group_name  = "testrsv"
-    storage_account_name = "sdvssfgegfew"
+    resource_group_name  = "tf-state"
+    storage_account_name = "satfstate04112025 "
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
     use_oidc             = true
