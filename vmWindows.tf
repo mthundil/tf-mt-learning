4cbf664360122a8ee03bfb371660f36ba18106b3
mthundil <85497016+mthundil@users.noreply.github.com>
2026-01-06 10:17:10 -0500

    Add Azure Windows Virtual Machine resource

---
+++ vmWindows.tf
@@ -0,0 +1,26 @@
+resource "azurerm_windows_virtual_machine" "main" {
+name = "${var.prefix}-vm"
+admin_username = "azureuser"
+admin_password = random_password.password.result
+location = azurerm_resource_group.rg.location
+resource_group_name = azurerm_resource_group.rg.name
+network_interface_ids = [azurerm_network_interface.nic.id]
+size = "Standard_DS1_v2"
+
+os_disk {
+name = "myOsDisk"
+caching = "ReadWrite"
+storage_account_type = "Premium_LRS"
+}
+
+source_image_reference {
+publisher = "MicrosoftWindowsServer"
+offer = "WindowsServer"
+sku = "2022-datacenter-azure-edition"
+version = "latest"
+}
+
+boot_diagnostics {
+storage_account_uri = azurerm_storage_account.diag.primary_blob_endpoint
+}
+}
